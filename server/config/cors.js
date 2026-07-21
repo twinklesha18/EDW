f60db3c531a197c8dfde6b10d9f3c94617a07282
twinklesha18 <twinklesha18@users.noreply.github.com>
2026-07-22 00:02:36 +0530

    Fix production CORS

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,19 +1,48 @@
 import { env } from './env.js'
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'https://edw-phi.vercel.app',
+  ...env.clientOrigins
+].map((origin) => origin.replace(/\/$/, ''))
+
 export const corsOptions = {
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  origin(origin, callback) {
-    const normalizedOrigin = origin?.replace(/\/$/, '')
 
-    if (!origin || env.clientOrigins.includes(normalizedOrigin)) {
-      callback(null, true)
-      return
+  methods: [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS'
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization'
+  ],
+
+  origin(origin, callback) {
+    // Allow Postman and server-to-server requests
+    if (!origin) {
+      return callback(null, true)
     }
 
-    const error = new Error('Origin is not allowed by CORS')
+    const normalizedOrigin = origin.replace(/\/$/, '')
+
+    if (allowedOrigins.includes(normalizedOrigin)) {
+      return callback(null, true)
+    }
+
+    const error = new Error(
+      `Origin is not allowed by CORS: ${normalizedOrigin}`
+    )
+
     error.statusCode = 403
-    callback(error)
+    return callback(error)
   },
+
+  optionsSuccessStatus: 204
 }
